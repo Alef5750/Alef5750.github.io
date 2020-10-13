266e07b838c2b7bbd63b79f9d9a2c8aba41f6058
Alef Lewitt <66555084+Alef1990@users.noreply.github.com>
2020-10-13 11:46:21 +0300

    Animations and more

--- Lewitt_Alef_ITCPreCourse/portfolio.js
+++ Lewitt_Alef_ITCPreCourse/portfolio.js
@@ -10,10 +10,32 @@ fetch(GITHUB_URL)
     console.log(data);
   });
 
-// card animation upon clicking it
-function fade() {
-  const card = document.querySelector(".card");
-  card.style.opacity = "0.5";
+// card fading animation upon clicking it
+const card = document.querySelector(".card");
+const itcText = document.querySelector("h2");
+const myName = document.getElementById("my-name");
+const backdrop = document.getElementById("backdrop");
+const linkedin = document.getElementById("linkedin");
+const github = document.getElementById("github");
+const stack = document.getElementById("stack");
+
+function expand() {
+  card.style.padding = "50px 400px";
+  card.style.backgroundImage = "url(sunrise.jpeg)";
+  itcText.style.color = "#DD9319";
+  myName.style.color = "#0272BD";
+  backdrop.style.backgroundColor = "#DD9319";
+  linkedin.style.color = "white";
+  github.style.color = "white";
+  stack.style.color = "white";
 }
 
-// POPMOTION PHYSICS
+// "This page was built with HTML, CSS, and JAVASCRIPT"
+let languages = ["HTML", "CSS", "JavaScript"];
+document.write(
+  `This page was built using
+    ${languages[0]}
+    ${languages[1]} 
+    and
+    ${languages[2]}`
+);
